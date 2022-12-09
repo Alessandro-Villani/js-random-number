88bd37778e81e58225cb8f3f3c58ff05120508a1
Alessandro-Villani <alessvill@libero.it>
2022-12-09 16:07:23 +0100

    picked input and buttons in js

--- js/script.js
+++ js/script.js
@@ -14,3 +14,18 @@ BONUS: Create un bottone reset che svuota i valori negli input e cancella i box 
 
 console.log('JS OK');
 
+//1. Picking Input
+
+const inputBoxNumber = document.getElementById('box-number');
+console.log(inputBoxNumber);
+const inputMin = document.getElementById('min');
+console.log(inputMin);
+const inputMax = document.getElementById('max');
+console.log(inputMax);
+
+//2. Picking Buttons
+
+const buttonGenerate = document.getElementById('generate');
+console.log(buttonGenerate);
+const buttonReset = document.getElementById('reset');
+console.log(buttonReset);
